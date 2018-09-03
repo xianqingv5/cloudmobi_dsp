6984618c442675a7eb0faeb75a62dd7e35e2fbdb
ERVee <736354882@qq.com>
2018-09-03 19:51:24 +0800

    上传图片视频功能开发
    2018年09月03日19:51:14

--- backend/assets/CommonAsset.php
+++ backend/assets/CommonAsset.php
@@ -24,6 +24,8 @@ class CommonAsset extends AssetBundle
 	public $js = [
 		'js/vue.2.5.17.js',
 		'plugin/element-ui@2.4.6/index.js',
+		'plugin/aws/aws-sdk-2.283.1.min.js',
+		'plugin/aws/app.js'
 	];
 
 	/**

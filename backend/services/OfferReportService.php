4baf19ee8ffe8908b6723ae67c1f9ec56e99de06
quettao <quettao>
2018-09-19 14:13:30 +0800

    offer id

--- backend/services/OfferReportService.php
+++ backend/services/OfferReportService.php
@@ -213,7 +213,8 @@ class OfferReportService extends BaseService
                 // offer id 组装
                 self::$res['status'] = 1;
                 foreach ($result as $k=>$v) {
-                    self::$res['data']['campaigns'][] = $v['channel'] . '_' . Yii::$app->params['OFFER_ID_STRING'] . str_pad( $v['id'], 3, 0, STR_PAD_LEFT );
+                    self::$res['data']['campaigns'][$k]['name'] = $v['channel'] . '_' . Yii::$app->params['OFFER_ID_STRING'] . str_pad( $v['id'], 3, 0, STR_PAD_LEFT );
+                    self::$res['data']['campaigns'][$k]['id'] = $v['id'];
                 }
             }
 

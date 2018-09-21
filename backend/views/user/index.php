6ea74b7fbf76bb84c1e2cc1ed0ae17d3d78f9ca6
ERVee <736354882@qq.com>
2018-09-21 10:43:29 +0800

    user index change
    2018年09月21日10:43:18

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -272,18 +272,41 @@
           type: 'get',
           success: function (result) {
             if (result.status === 1) {
-              result.data.map(function (ele) {
-                vm.ruleForm.roleOptions.push({
-                  label: ele.group_name,
-                  value: ele.id
-                })
-              })
+              vm.ruleForm.roleOptionsBase = result.data
+              // result.data.map(function (ele) {
+              //   vm.ruleForm.roleOptions.push({
+              //     label: ele.group_name,
+              //     value: ele.id
+              //   })
+              // })
               if (result.data.length === 1) {
                 vm.ruleForm.role = result.data[0].id
               }
             }
           }
         })
+      },
+      setRoleOptions (type) {
+        var that = this
+        this.ruleForm.roleOptions.splice(0)
+        if (type === 'create') {
+          this.ruleForm.roleOptionsBase.map(function (ele) {
+            if (ele.status === 1) {
+              that.ruleForm.roleOptions.push({
+                label: ele.group_name,
+                value: ele.id
+              })
+            }
+          })
+        }
+        if (type === 'edit') {
+          this.ruleForm.roleOptionsBase.map(function (ele) {
+            that.ruleForm.roleOptions.push({
+              label: ele.group_name,
+              value: ele.id
+            })
+          })
+        }
       },
       resetForm(formName) {
         if (this.$refs[formName] !== undefined) {
@@ -296,6 +319,7 @@
         }
       },
       showDialog (type,  item) {
+        this.setRoleOptions(type)
         this.dialogVisible = true
         this.dialogBus.type = type
         if (type === 'create') {

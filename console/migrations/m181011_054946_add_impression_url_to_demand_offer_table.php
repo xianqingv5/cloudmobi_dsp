f1846b718966cb235ef563fa70fb2c415cc44f0d
quettao <quettao>
2018-10-11 14:10:57 +0800

    增加字段到demand offer table

---
+++ console/migrations/m181011_054946_add_impression_url_to_demand_offer_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181011_054946_add_impression_url_to_demand_offer_table
+ */
+class m181011_054946_add_impression_url_to_demand_offer_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn(
+            '{{%demand_offers}}',
+            'open_type',
+            $this->string(2000)->notNull()->defaultValue('')->comment('json格式:impression跳转url')->after('comment')
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m181011_054946_add_impression_url_to_demand_offer_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m181011_054946_add_impression_url_to_demand_offer_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
